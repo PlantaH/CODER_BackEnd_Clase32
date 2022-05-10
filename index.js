1b3ed07adac2625569225377480dbc4b08b4be86
PlantaH <laplantah@gmail.com>
2022-05-10 13:02:29 -0300

    make it better

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(session({
 }))
 
 //Midelware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public')); //app.use(express.static(__dirname + '/public'));
 app.use(express.json()); // body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
